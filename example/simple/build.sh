70ad4ee5ba8fea84341ee53584c6dfb882697c81
inbei <k.in@live.cn>
2016-05-20 12:54:31 +0800

    Update build.sh
    Linux 下编译失败

--- example/simple/build.sh
+++ example/simple/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-g++ -o ../../bin/producer Producer.cpp -g -m64 -I../../include -L../../bin -lrocketmq64 
+g++ -g -std=c++11 -o ../../bin/producer Producer.cpp -g -m64 -I../../include -L../../bin -lrocketmq64 -lssl -lcrypto -lcurl
 
-g++ -o ../../bin/pullconsumer PullConsumer.cpp -m64 -g -I../../include -L../../bin -lrocketmq64 
+g++ -g -std=c++11 -o ../../bin/pullconsumer PullConsumer.cpp -m64 -g -I../../include -L../../bin -lrocketmq64 -lssl -lcrypto -lcurl
 
-g++ -o ../../bin/pushconsumer PushConsumer.cpp -m64 -g -I../../include -L../../bin -lrocketmq64 
+g++ -g -std=c++11 -o ../../bin/pushconsumer PushConsumer.cpp -m64 -g -I../../include -L../../bin -lrocketmq64 -lssl -lcrypto -lcurl
 
